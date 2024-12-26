9a00e180b5a4cbda091bcaa0def9d60532feee96
zonvid <fpack@163.com>
2024-12-26 14:18:53 +0800

    server use gnet lib

---
+++ example/echoServer/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+
+	"github.com/panjf2000/gnet/v2"
+)
+
+type echoServer struct {
+	*gnet.BuiltinEventEngine
+	address string
+}
+
+// OnBoot is triggered when the server starts.
+func (es *echoServer) OnBoot(eng gnet.Engine) gnet.Action {
+	log.Printf("Echo server is listening on %s\n", es.address)
+	return gnet.None
+}
+
+// OnShutdown is triggered when the server is shutting down.
+func (es *echoServer) OnShutdown(eng gnet.Engine) {
+	log.Println("Echo server is shutting down")
+}
+
+// OnOpen is triggered when a new connection is opened.
+func (es *echoServer) OnOpen(conn gnet.Conn) ([]byte, gnet.Action) {
+	log.Printf("New connection from %s\n", conn.RemoteAddr().String())
+	return nil, gnet.None
+}
+
+// OnClose is triggered when a connection is closed.
+func (es *echoServer) OnClose(conn gnet.Conn, err error) gnet.Action {
+	log.Printf("Connection from %s closed\n", conn.RemoteAddr().String())
+	return gnet.None
+}
+
+var (
+	echotag = []byte("echo:")
+)
+
+// OnTraffic is triggered when there is data to read from the connection.
+func (es *echoServer) OnTraffic(conn gnet.Conn) gnet.Action {
+	// Read incoming data
+	buffer, _ := conn.Next(-1)
+
+	// Log received data
+	log.Printf("Received data: %s", string(buffer))
+
+	// Echo the data back to the client
+	sendBuf := make([]byte, len(buffer)+len(echotag))
+	copy(sendBuf, echotag)
+	copy(sendBuf[len(echotag):], buffer)
+	conn.Write(sendBuf)
+
+	// Return no action to continue the connection
+	return gnet.None
+}
+
+func main() {
+	// Address to bind the server
+	address := "unix:///tmp/codesocket.tmp"
+	server := &echoServer{address: address}
+
+	// Start the server
+	err := gnet.Run(server, address, gnet.WithMulticore(true), gnet.WithReusePort(true))
+	if err != nil {
+		log.Fatalf("Failed to start server: %v\n", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,18 @@ module unixsocket
 
 go 1.23.2
 
-require golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67
+require (
+	github.com/panjf2000/gnet/v2 v2.6.3
+	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67
+)
+
+require (
+	github.com/panjf2000/ants/v2 v2.10.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+)
